cdae2423a2747f89cfea76d554486faa86b0b3df
SamirIBM <48705781+SamirIBM@users.noreply.github.com>
2021-11-07 12:44:15 +0530

    message

--- src/pack/Assignment2.java
+++ src/pack/Assignment2.java
@@ -9,6 +9,7 @@ public class Assignment2 {
 
 //Assignment 2 updated
 //Git desktop version
+//Fixing the Assignment2.java
 	public static void main(String[] args)throws InterruptedException {
 		// TODO Auto-generated method stub
 
